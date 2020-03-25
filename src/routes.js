f46145b98608295f5ea4e9eb044800e7452e8857
eberjoe <eberjoe@gmail.com>
2020-03-25 20:06:17 -0300

    Add controller layer

--- src/routes.js
+++ src/routes.js
@@ -1,32 +1,12 @@
 const express = require('express');
-const crypto = require('crypto');
-const connection = require('./database/connection');
+
+const OngController = require('./controllers/OngController');
 
 // desacopla o módulo de rotas do Express em uma variável
 const routes = express.Router();
 
-routes.get('/ongs', async (request, response) => {
-    const ongs = await connection('ongs').select('*');
-
-    return response.json(ongs);
-});
-
-routes.post('/ongs', async (request, response) => {
-    const { name, email, whatsapp, city, uf } = request.body;
-
-    const id = crypto.randomBytes(4).toString('HEX');
-
-    await connection('ongs').insert({
-        id,
-        name,
-        email,
-        whatsapp,
-        city,
-        uf,
-    });
-
-    return response.json({ id });
-});
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
 // comando do node para disponibilizar variável para o exterior
 module.exports = routes;
