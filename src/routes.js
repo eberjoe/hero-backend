e6bb0d91613ec5e7e936b659e7ce777f45326ebd
eberjoe <eberjoe@gmail.com>
2020-03-26 22:38:14 -0300

    Add routes for incidents

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,21 @@
 const express = require('express');
 
 const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
 
 // desacopla o módulo de rotas do Express em uma variável
 const routes = express.Router();
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
 
 // comando do node para disponibilizar variável para o exterior
 module.exports = routes;
