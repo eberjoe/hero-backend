35e5e3c83363755022ff2ae7da252ad5c2db4247
eberjoe <eberjoe@gmail.com>
2020-03-26 23:45:17 -0300

    Add login

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,12 @@ const express = require('express');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 // desacopla o módulo de rotas do Express em uma variável
 const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
